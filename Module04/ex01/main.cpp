89b7b054aa5bef2276c1a07dc48f5e31d939b911
Jingran Lyu <jlyu@F2N050.42adel.org.au>
2023-09-19 17:34:54 +0930

    edit module04

--- Module04/ex01/main.cpp
+++ Module04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jlyu <jlyu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 12:29:10 by jlyu              #+#    #+#             */
-/*   Updated: 2023/09/19 16:59:26 by jlyu             ###   ########.fr       */
+/*   Updated: 2023/09/19 17:31:48 by jlyu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,19 @@
 
 int main()
 {
-	const Animal *lotsOfAnimals[10];
-	for (int i = 0; i < 10; i++)
-	{
-		if (i < 10 / 2)
-			lotsOfAnimals[i] = new Dog();
-		else
-			lotsOfAnimals[i] = new Cat();
-	}
+	const Animal *j = new Dog();
+	const Animal *i = new Cat();;
 	std::cout << "-------------------------------------\n";
-	std::cout << lotsOfAnimals[0]->getType() << std::endl;
-	std::cout << lotsOfAnimals[5]->getType() << std::endl;
+	std::cout << j->getType() << std::endl;
+	std::cout << i->getType() << std::endl;
 	Brain *brain;
-	brain = &lotsOfAnimals[0]->getBrain();
+	brain = &j->getBrain();
 	brain->setIdea("Hello World!", 0);
 	brain->setIdea("Let's play ball!!", 1);
 	brain->setIdea("Feed!", 2);
-	std::cout << lotsOfAnimals[0]->getBrain().getIdea(0) << std::endl;
-	std::cout << lotsOfAnimals[0]->getBrain().getIdea(1) << std::endl;
+	std::cout << j->getBrain().getIdea(0) << std::endl;
+	std::cout << j->getBrain().getIdea(1) << std::endl;
 	std::cout << "-------------------------------------\n";
-	for (int i = 0; i < 10; i++)
-		delete lotsOfAnimals[i];
+	delete j;
+	delete i;
 }
